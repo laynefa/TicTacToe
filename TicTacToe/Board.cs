74ec85f4a2f41f6bbf1c47759edc5528a2e772dd
laynefa <35180572+laynefa@users.noreply.github.com>
2020-08-08 21:45:53 -0400

    Refactoring

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        char[,] board;
+        public bool gameComplete;
+        int size;
+        int spaces;
+        public int turns;
+        List<int> options;
+        Random rand = new System.Random();
+
+        public Board(int gameSize)
+        {
+            size = gameSize;
+            board = new char[size, size];
+            spaces = size * size;
+            options = Enumerable.Range(0, spaces).ToList();
+            gameComplete = false;
+            turns = 0;
+            PrintBoard(true);
+        }
+
+        public void PrintBoard(bool referenceBoard = false)
+        {
+            string printedBoard = "";
+            string data;
+            int count = 0;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                printedBoard += " | ";
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (referenceBoard)
+                    {
+                        count++;
+                        data = count.ToString();
+                    }
+                    else
+                        data = board[i, j].ToString();
+                    printedBoard = printedBoard + data + " | ";
+                }
+                printedBoard += "\n";
+            }
+            Console.WriteLine(printedBoard);
+        }
+
+        private bool IsPositionTaken(int position)
+        {
+            return !options.Contains(position);
+        }
+
+        private bool RemainingOptions()
+        {
+            if (options.Count == 0)
+            {
+                PrintBoard();
+                Console.WriteLine("Game over, tie.");
+                return false;
+            }
+            return true;
+        }
+
+        public bool PlacePosition(int position, char symbol)
+        {
+            if (IsPositionTaken(position))
+                return false;
+            turns++;
+            int row = position / size;
+            int column = position % size;
+            Console.WriteLine(row + " " + column);
+            board[row, column] = symbol;
+            options.Remove(position);
+            gameComplete = GameWon(position) || !RemainingOptions();
+            return true;
+        }
+
+        public void PlaceCpuPosition(char symbol)
+        {
+            int position = options[rand.Next(0, options.Count)];
+            options.Remove(position);
+            int row = position / size;
+            int column = position % size;
+            board[row, column] = 'o';
+        }
+
+        public bool GameWon(int position)
+        {
+            int row = position / size;
+            int col = position % size;
+            char value = board[row, col];
+            gameComplete = true;
+            for (int i = 0; i < board.GetLength(1); i++)
+            {
+                // walk forwards through columns for current row
+                if (value != board[row, i])
+                {
+                    gameComplete = false;
+                    break;
+                }
+            }
+            if (gameComplete)
+                return gameComplete;
+            gameComplete = true;
+            // if still false then need to check vertical
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                // walk forwards through rows for current column
+                if (value != board[i, col])
+                {
+                    gameComplete = false;
+                    break;
+                }
+            }
+            if (gameComplete)
+                return gameComplete;
+            gameComplete = true;
+            // if still false need to check diagonals
+            // check top left to bottom right
+            for (int i = 0; i < size; i++)
+            {
+                if (value != board[i, i])
+                {
+                    gameComplete = false;
+                    break;
+                }
+            }
+            if (gameComplete)
+                return gameComplete;
+            gameComplete = true;
+            // check top right to bottom left
+            int tempRow = 0;
+            for (int j = size - 1; j >= 0; j--)
+            {
+                if (value != board[tempRow, j])
+                {
+                    gameComplete = false;
+                    break;
+                }
+                tempRow++;
+            }
+            if (gameComplete)
+                return gameComplete;
+            return false;
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Drawing;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
 
 namespace TicTacToe
 {
@@ -11,169 +12,54 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            int position;
-            char[,] board = InitializeGame();
-            int gameSize = board.GetLength(0);
+            Board board = InitializeGame();
             bool gameComplete = false;
-            int row;
-            int column;
-            List<int> options = Enumerable.Range(0, gameSize * gameSize).ToList();
-            var rand = new System.Random();
+            int position;
             while (!gameComplete)
             {
-                //player choice
                 Console.Write("Choose your position: ");
                 position = Int32.Parse(Console.ReadLine()) - 1;
-                if (!options.Contains(position))
+                if (!board.PlacePosition(position, 'x'))
                     Console.WriteLine("Position taken. Please choose another position.");
                 else
                 {
-                    row = position / gameSize;
-                    column = position % gameSize;
-                    Console.WriteLine(row + " " + column);
-                    board[row, column] = 'x';
-                    options.Remove(position);
-                    if (GameWon(row, column, board, gameSize))
+                    if (board.gameComplete)
                     {
-                        Console.WriteLine(PrintBoard(board));
+                        board.PrintBoard();
                         Console.WriteLine("x won");
-                        gameComplete = true;
-                        Console.Write("Play Again? ");
-                        if (Console.ReadLine() == "y")
-                            gameComplete = false;
+                        gameComplete = RestartGame();
                         board = InitializeGame();
-                        gameSize = board.GetLength(0);
-                        options = Enumerable.Range(0, gameSize * gameSize).ToList();
                     }
                     else
                     {
-                        if (options.Count == 0)
+                        board.PlaceCpuPosition('o');
+                        board.PrintBoard();
+                        if (board.gameComplete)
                         {
-                            Console.WriteLine(PrintBoard(board));
-                            Console.WriteLine("Game over, tie.");
-                            Console.Write("Play Again? ");
-                            if (Console.ReadLine() == "y")
-                                gameComplete = false;
+                            Console.WriteLine("o won");
+                            gameComplete = RestartGame();
                             board = InitializeGame();
-                            gameSize = board.GetLength(0);
-                            options = Enumerable.Range(0, gameSize * gameSize).ToList();
-                        }
-                        else
-                        {
-                            //cpu choice
-                            position = options[rand.Next(0, options.Count)];
-                            options.Remove(position);
-                            row = position / gameSize;
-                            column = position % gameSize;
-                            board[row, column] = 'o';
-                            Console.WriteLine(PrintBoard(board));
-
-                            if (GameWon(row, column, board, gameSize))
-                            {
-                                Console.WriteLine("o won");
-                                gameComplete = true;
-                                Console.Write("Play Again? ");
-                                if (Console.ReadLine() == "y")
-                                    gameComplete = false;
-                                board = InitializeGame();
-                                gameSize = board.GetLength(0);
-                                options = Enumerable.Range(0, gameSize * gameSize).ToList();
-                            }
                         }
                     }
                 }
             }
         }
 
-        static char[,] InitializeGame()
+        static Board InitializeGame()
         {
-            Console.Write("Pick size of game: ");
-            int gameSize = Int32.Parse(Console.ReadLine());
-            char[,] board = new char[gameSize, gameSize];
-            Console.WriteLine(PrintBoard(board, true));
+            int gameSize;
+            Console.Write("Pick game size: ");
+            gameSize = Int32.Parse(Console.ReadLine());
+            Board board = new Board(gameSize);
             return board;
         }
 
-        static string PrintBoard(char[,] board, bool referenceBoard=false)
+        static bool RestartGame()
         {
-            string printedBoard = "";
-            string data;
-            int count = 0;
-            for (int i = 0; i < board.GetLength(0); i++)
-            {
-                printedBoard += " | ";
-                for (int j = 0; j < board.GetLength(1); j++)
-                {
-                    if (referenceBoard)
-                    {
-                        count++;
-                        data = count.ToString();
-                    }
-                    else
-                        data = board[i, j].ToString();
-                    printedBoard = printedBoard + data + " | ";
-                }
-                printedBoard += "\n";
-            }
-            return printedBoard;
-        }
-
-        static bool GameWon(int row, int col, char[,] board, int gameSize)
-        {
-            char value = board[row, col];
-            bool gameWon = true;
-            for (int i = 0; i < board.GetLength(1); i++)
-            {
-                // walk forwards through columns for current row
-                if (value != board[row, i])
-                {
-                    gameWon = false;
-                    break;
-                }
-            }
-            if (gameWon)
-                return gameWon;
-            gameWon = true;
-            // if still false then need to check vertical
-            for (int i = 0; i < board.GetLength(0); i++)
-            {
-                // walk forwards through rows for current column
-                if (value != board[i, col])
-                {
-                    gameWon = false;
-                    break;
-                }
-            }
-            if (gameWon)
-                return gameWon;
-            gameWon = true;
-            // if still false need to check diagonals
-            // check top left to bottom right
-            for (int i = 0; i < gameSize; i++)
-            {
-                if (value != board[i, i])
-                {
-                    gameWon = false;
-                    break;
-                }
-            }
-            if (gameWon)
-                return gameWon;
-            gameWon = true;
-            // check top right to bottom left
-            int tempRow = 0;
-            for (int j = gameSize - 1; j >= 0; j--)
-            {
-                if (value != board[tempRow, j])
-                {
-                    gameWon = false;
-                    break;
-                }
-                tempRow++;
-            }
-            if (gameWon)
-                return gameWon;
-            return false;
+            Console.Write("Play Again? ");
+            if (Console.ReadLine() == "y")
+                return false;
+            return true;
         }
     }
 }
